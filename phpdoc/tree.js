63b0fce4fb9eee820668f27de774e38ecc08590d
piwi <me@e-piwi.fr>
2015-01-01 19:51:07 +0100

    new documentation generation

--- phpdoc/tree.js
+++
@@ -1,7 +0,0 @@
-var tree = [["GitApi","GitApi.html","",
-                [["GitApi","GitApi\/GitApi.html","",
-                [        ]],["GitCommand","GitApi\/GitCommand.html"," < Command",
-                [        ]],["GitRuntimeException","GitApi\/GitRuntimeException.html"," < RuntimeException",
-                [        ]],["Repository","GitApi\/Repository.html","",
-                [        ]]        ]]        ]
-
